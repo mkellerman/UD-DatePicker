8a9a3c6d1b1cd2a4f57448dedba5b0bdbba6f9fe
Adam <adambacon1@hotmail.com>
2019-10-26 16:50:49 +0100

    showing side-by-side the two components

--- DayPickerExample.ps1
+++ DayPickerExample.ps1
@@ -1,10 +1,12 @@
 Import-Module -Name UniversalDashboard.Community
 Import-Module UniversalDashboard.DayPicker
+Import-Module UniversalDashboard.WeekPicker
 Get-UDDashboard | Stop-UDDashboard
 Start-UDDashboard -Port 10005 -Dashboard (
     New-UDDashboard -Title "Powershell UniversalDashboard" -Content {
         New-UDLayout -Columns 2 -Content {
             New-DayPicker
+            New-WeekPicker
         }
     }
 )
